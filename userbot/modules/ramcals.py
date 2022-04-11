9bd6cc7f56021d3c60200e314ff9de1be39df897
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-04-12 01:34:47 +0700

    Update ramcals.py

--- userbot/modules/ramcals.py
+++ userbot/modules/ramcals.py
@@ -7,6 +7,9 @@
 # Ambil aja gapapa tp Gaguna kaya hidup lu Woakkakaka
 
 
+from telethon.utils import get_display_name
+from telethon.tl import types
+
 from telethon.tl.functions.channels import GetFullChannelRequest as getchat
 from telethon.tl.functions.phone import CreateGroupCallRequest as startvc
 from telethon.tl.functions.phone import DiscardGroupCallRequest as stopvc
@@ -14,11 +17,12 @@ from telethon.tl.functions.phone import EditGroupCallTitleRequest as settitle
 from telethon.tl.functions.phone import GetGroupCallRequest as getvc
 from telethon.tl.functions.phone import InviteToGroupCallRequest as invitetovc
 
-from telethon.tl.types import ChatAdminRights
 from userbot import CMD_HELP
 from userbot.events import register
+from userbot import CMD_HANDLER as cmd
+from userbot.utils import edit_delete, edit_or_reply, ram_cmd
 
-NO_ADMIN = "`WOY HINA LU BUKAN ADMIN NGENTOT!!`"
+NO_ADMIN = "`LU BUKAN ADMIN NGENTOT!!`"
 
 def vcmention(user):
     full_name = get_display_name(user)
@@ -27,10 +31,10 @@ def vcmention(user):
     return f"[{full_name}](tg://user?id={user.id})"
 
 
-async def get_call(event):
-    kybot = await event.client(getchat(event.chat_id))
-    rizky = await event.client(getvc(kybot.full_chat.call, limit=1))
-    return rizky.call
+async def get_call(komtol):
+    rambot = await komtol.client(getchat(komtol.chat_id))
+    rama = await komtol.client(getvc(rambot.full_chat.call, limit=1))
+    return rama.call
 
 
 def user_list(l, n):
@@ -38,64 +42,97 @@ def user_list(l, n):
         yield l[i: i + n]
 
 
-@register(outgoing=True, pattern=r"^\.startvc$")
-async def start_voice(kybot):
-    chat = await kybot.get_chat()
+@ram_cmd(pattern="startvc$")
+@register(pattern=r"^\.cstr$", sudo=True)
+async def start_voice(c):
+    await c.client.get_me()
+    chat = await c.get_chat()
     admin = chat.admin_rights
     creator = chat.creator
 
     if not admin and not creator:
-        await kybot.edit(f"**Maaf {ALIVE_NAME} Bukan Admin 👮**")
+        await edit_delete(c, "**LO BUKAN ADMIN ANJINGGG!!!!!!**")
         return
     try:
-        await kybot.client(startvc(kybot.chat_id))
-        await kybot.edit("`OS UDAH DIBUKA NGENTOT, KALO UDAH DI BUKA JANGAN ON CAM PAMERIN MUKA LU YANG MIRIP BABI HUTAN YAH BANGSAT!`")
+        await c.client(startvc(c.chat_id))
+        await edit_or_reply(c, "`OBROLAN SUARA DI NYALAKAN, YANG ONCAM LO NGENTOT!!`")
     except Exception as ex:
-        await kybot.edit(f"**ERROR:** `{ex}`")
+        await edit_delete(c, f"**ERROR:** `{ex}`")
 
 
-@register(outgoing=True, pattern=r"^\.stopvc$")
-async def stop_voice(rizky):
-    chat = await rizky.get_chat()
+@ram_cmd(pattern="stopvc$")
+@register(pattern=r"^\.cstp$", sudo=True)
+async def stop_voice(c):
+    await c.client.get_me()
+    chat = await c.get_chat()
     admin = chat.admin_rights
     creator = chat.creator
 
     if not admin and not creator:
-        await rizky.edit(f"**Maaf {ALIVE_NAME} Bukan Admin 👮**")
+        await edit_delete(c, "**lo bukan admin ngentoootttttt!!!**")
         return
     try:
-        await rizky.client(stopvc(await get_call(rizky)))
-        await rizky.edit("`OS NYA DIMATIIN NORAK, TYPING AJA TYPING SABAN HARI OS MULU GA CAPE TU MULUT YA NGENTOT!`")
+        await c.client(stopvc(await get_call(c)))
+        await edit_or_reply(c, "`OBROLAN SUARA DI MATIKAN, TYPING AJA YA BABI!!!!`")
     except Exception as ex:
-        await rizky.edit(f"**ERROR:** `{ex}`")
+        await edit_delete(c, f"**ERROR:** `{ex}`")
 
-@register(outgoing=True, pattern=r"^\.vcinvite", groups_only=True)
-async def _(rambot):
-    await rambot.edit("`Memulai Invite member group...`")
+
+@ram_cmd(pattern="vcinvite")
+async def _(c):
+    xxnx = await edit_or_reply(c, "`Inviting Members to Voice Chat...`")
     users = []
     z = 0
-    async for x in rambot.client.iter_participants(rambot.chat_id):
+    async for x in c.client.iter_participants(c.chat_id):
         if not x.bot:
             users.append(x.id)
-    hmm = list(user_list(users, 6))
-    for p in hmm:
+    botman = list(user_list(users, 6))
+    for p in botman:
         try:
-            await rambot.client(invitetovc(call=await get_call(rambot), users=p))
+            await c.client(invitetovc(call=await get_call(c), users=p))
             z += 6
         except BaseException:
             pass
-    await rambot.edit(f"`Menginvite {z} Member`")
+    await xxnx.edit(f"`{z}` **Orang Berhasil diundang ke VCG**")
+
+
+@ram_cmd(pattern="vctitle(?: |$)(.*)")
+@register(pattern=r"^\.cvct$", sudo=True)
+async def change_title(e):
+    title = e.pattern_match.group(1)
+    me = await e.client.get_me()
+    chat = await e.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+
+    if not title:
+        return await edit_delete(e, "**Silahkan Masukan Title Obrolan Suara Grup**")
+
+    if not admin and not creator:
+        await edit_delete(e, f"**Maaf {me.first_name} Bukan Admin 👮**")
+        return
+    try:
+        await e.client(settitle(call=await get_call(e), title=title.strip()))
+        await edit_or_reply(e, f"**Berhasil Mengubah Judul VCG Menjadi** `{title}`")
+    except Exception as ex:
+        await edit_delete(e, f"**ERROR:** `{ex}`")
 
 
 CMD_HELP.update(
     {
-        "ramcalls": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.startvc`\
-         \n↳ : Memulai Obrolan Suara dalam Group.\
-         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.stopvc`\
-         \n↳ : `Menghentikan Obrolan Suara Pada Group.`\
-         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.vcinvite`\
-         \n↳ : Invite semua member yang berada di group. (Kadang bisa kadang kaga).\
-         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.joinvc` & `.leavevc`\
-         \n↳ : Membuat Obrolan Suara Secara Fake."
+        "vcg": f"**Plugin : **`vcg`\
+        \n\n  •  **Syntax :** `{cmd}startvc`\
+        \n  •  **Function : **Untuk Memulai voice chat group\
+        \n\n  •  **Syntax :** `{cmd}stopvc`\
+        \n  •  **Function : **Untuk Memberhentikan voice chat group\
+        \n\n  •  **Syntax :** `{cmd}vctitle` <title vcg>\
+        \n  •  **Function : **Untuk Mengubah title/judul voice chat group\
+        \n\n  •  **Syntax :** `{cmd}vcinvite`\
+        \n  •  **Function : **Mengundang Member group ke voice chat group\
+        \n\n  • **Syntax  :** `{cmd}jvc`\
+        \n  •  **Function : **Naik ke Obrolan Suara Palsu\
+        \n\n  • **Syntax  :** `{cmd}lvc`\
+        \n  •  **Function : **Turun Dari Obrolan Suara Palsu\
+    "
     }
 )
