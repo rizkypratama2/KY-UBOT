6acdc719e659d190afa06c3b7fbd077631f2e831
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-04-12 02:26:39 +0700

    puki

--- userbot/modules/sange.py
+++ userbot/modules/sange.py
@@ -17,7 +17,7 @@ async def _(event):
         asupannya = [
             asupan
             async for asupan in event.client.iter_messages(
-                "@cari_temen_bangsat", filter=InputMessagesFilterVideo
+                "@gadistiktokmantap", filter=InputMessagesFilterVideo
             )
         ]
         aing = await event.client.get_me()
